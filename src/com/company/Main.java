553a08335aa4727aa593283d177cb3d6ecaf0a62
zritchey <zachrritchey@gmail.com>
2016-12-21 10:32:29 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int [][]a={{1,2,-2,0},{-3,4,7,2},{6,0,3,1}};
+        int [][]b={{-1,3},{0,9},{1,-11},{4,-5}};
+        System.out.println(MatrixMult.mult(a,b));
+
+
+    }
+}
---
+++ src/com/company/MatrixMult.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by zr162 on 12/21/16.
+ */
+public class MatrixMult {
+    static public int [][]mult(int[][]a,int[][]b){
+        
+    }
+}
