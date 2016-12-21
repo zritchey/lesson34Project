c71df7e9ba2c6c456b9e4e1968806484fe8b5f0a
zachary <zachrritchey@gmail.com>
2016-12-21 17:11:13 -0500

    hi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,20 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         int [][]a={{1,2,-2,0},{-3,4,7,2},{6,0,3,1}};
         int [][]b={{-1,3},{0,9},{1,-11},{4,-5}};
-        System.out.println(MatrixMult.mult(a,b));
-
+        int[][]end=MatrixMult.mult(a,b);
+        for(int i=0;i<end.length;i++){
+            for(int x=0;x<end[0].length;x++){
+                System.out.print(end[i][x]+"\t");
+            }
+            if(i<end.length-1)
+                System.out.println();
+        }
 
     }
 }
--- src/com/company/MatrixMult.java
+++ src/com/company/MatrixMult.java
@@ -5,6 +5,14 @@ package com.company;
  */
 public class MatrixMult {
     static public int [][]mult(int[][]a,int[][]b){
-        
+        int [][]mtrx=new int [a.length][b[0].length];
+        for(int r=0;r<a.length;r++){
+            for (int c=0;c<b[0].length;c++ ){
+                for (int i=0;i<b.length;i++) {
+                    mtrx[r][c] +=(a[r][i]*b[i][c]);
+                }
+            }
+        }
+        return mtrx;
     }
 }
